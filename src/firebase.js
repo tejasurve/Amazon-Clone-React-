43f7dda3fea5a297b222eaa0ed4341299a27bbe3
tejasurve <54415609+tejasurve@users.noreply.github.com>
2020-10-25 19:26:40 +0530

    Amazon Clone

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBpEYDsmeHNPeinZVrBBLoZVR1X7tS70m4",
+    authDomain: "clone-8f8c1.firebaseapp.com",
+    databaseURL: "https://clone-8f8c1.firebaseio.com",
+    projectId: "clone-8f8c1",
+    storageBucket: "clone-8f8c1.appspot.com",
+    messagingSenderId: "10887030371",
+    appId: "1:10887030371:web:fdd7d73fe854045fc83a56",
+    measurementId: "G-SY530F36K9"
+  };
